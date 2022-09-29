f3cf8384af5aa62d2149275c23266140c49f7a13
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2022-09-29 17:49:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+};
+
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+ 
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
+const toggle = document.getElementById('menu__burger');
+const menu = document.getElementById('menu');
+const sidebar = document.getElementById('fixed-panel');
+
+toggle.onclick = function() {
+  menu.classList.toggle('active');
+  toggle.classList.toggle('active');
+  sidebar.classList.toggle('active');
+  body.classList.toggle('hidden');
+};
+
+document.onclick = function(e) {
+  if (e.target.id !== 'menu__burger' && e.target.id !== 'menu' && e.target.id !== 'fixed-panel') {
+    menu.classList.remove('active');
+    toggle.classList.remove('active');
+    sidebar.classList.remove('active');
+    body.classList.remove('hidden');
+  }
+};
+
+const swiper = new Swiper('.first-screen__swiper', {
+  allowTouchMove: false,
+  effect: "cards",
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+});
+
+const projects = new Swiper('.projects__swiper', {
+  grabCursor: true,
+  width: 442,
+  freeMode: true,
+  // loop: true,
+  // loopedSlides: 4,
+});
+
+const partners = new Swiper('.partners__swiper', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    624: {
+      slidesPerView: 2,
+    },
+    994: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  }
+});
